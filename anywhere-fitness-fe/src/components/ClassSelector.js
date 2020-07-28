2df1c7cae402a54be42f635778ca8e0aaac38808
Parker williams <parkster246@gmail.com>
2020-07-28 10:53:48 -0500

    added classSelector component.

---
+++ anywhere-fitness-fe/src/components/ClassSelector.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import * as Yup from 'yup';
+import axios from 'axios';
+import styled from 'styled-components'
+
+function classSelector(props){
+const availableClasses = {
+    classes:''
+}
+}
+export default ClassSelector;
